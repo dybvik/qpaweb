ba3356cbf5ac677f8e016b7c4b80b9bbf73cc41d
Jon S E Jakobsen <jsjakobs@stud.hist.no>
2013-05-15 19:39:04 +0200

    Lagt til ExposeJSONP-metode

--- src/qpawebd/tornadoJSON.py
+++ src/qpawebd/tornadoJSON.py
@@ -1,6 +1,10 @@
+import pprint
 import tornado.ioloop
 import tornado.web
+import tornado.escape
 import urllib
+import json
+
 from src.lib import JSONGAPTranslate as JT
 
 class PostDemo(tornado.web.RequestHandler):
@@ -10,13 +14,45 @@ class PostDemo(tornado.web.RequestHandler):
         self.write("JobbID: " + self.get_argument('jobID'))
         self.write(name)
 
+## Slå hovedgreiene i Expose og ExposeJS sammen
 class Expose(tornado.web.RequestHandler):
     def get(self):
-        self.write("placeholder for JSON")
+        self.JT = JT.JSONGAPTranslate()
+        self.write(json.dumps(self.JT.Expose(),
+                              sort_keys=True,
+                              indent=4,
+                              separators=(',', ': ')))
+        self.set_header("Content-Type", "application/JSON")
+
+class ExposeJS(tornado.web.RequestHandler):
+    def get(self):
+        self.JT = JT.JSONGAPTranslate()
+        self.write("var expose = " + json.dumps(self.JT.Expose(),
+                                                sort_keys=True,
+                                                indent=4,
+                                                separators=(',', ': ')) + ";")
+        self.set_header("Content-Type", "application/javascript")
+
+
+class ExposeJSONP(tornado.web.RequestHandler):
+    def get(self):
+        self.JT = JT.JSONGAPTranslate()
+        self.write('capabilities(' +json.dumps(self.JT.Expose(),
+                                           sort_keys=True,
+                                           indent=4,
+                                           separators=(',', ': ')) + ')')
+        self.set_header("Content-Type", "application/javascript; charset=utf-8")
+
+class DumpPost(tornado.web.RequestHandler):
+    def post(self):
+        self.write("<pre>"+tornado.escape.url_unescape(self.request.body,'utf-8'))
 
 application = tornado.web.Application([
     (r"/", PostDemo),
-    (r"/expose/", Expose),
+    (r"/exposeJSON/", Expose),
+    (r"/exposeJS/", ExposeJS),
+    (r"/exposeJSONP/", ExposeJSONP),
+    (r"/dump/",DumpPost),
     ])
 
 if __name__ == "__main__":
