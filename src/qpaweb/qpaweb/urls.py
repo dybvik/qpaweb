2cff87c0eaf1f2a67b89e2e47c58723f20d6d494
Ole Gunnar Dybvik <olegd@tihlde.org>
2013-05-22 03:36:25 +0200

    Adder link to home.

--- src/qpaweb/qpaweb/urls.py
+++ src/qpaweb/qpaweb/urls.py
@@ -9,7 +9,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^dev/$', TemplateView.as_view(template_name="index.html")),
+    url(r'^dev/$', TemplateView.as_view(template_name="index.html"), name='index'),
     url(r'^test/$', TemplateView.as_view(template_name="quiver.html")),
                        url(r'^signup/$', TemplateView.as_view(template_name="signup.html"), name="signup"),
                        url(r'^help/$', TemplateView.as_view(template_name="help.html"), name="help"),
