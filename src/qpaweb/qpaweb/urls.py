290ccb6ad27775d54b5e9897923ff92be99b890d
Ole Gunnar Dybvik <olegd@tihlde.org>
2013-05-22 11:32:14 +0200

    Filled out some locales and about us.

--- src/qpaweb/qpaweb/urls.py
+++ src/qpaweb/qpaweb/urls.py
@@ -9,13 +9,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-<<<<<<< HEAD
-    url(r'^dev/$', TemplateView.as_view(template_name="index.html")),
-
-=======
     url(r'^dev/$', TemplateView.as_view(template_name="index.html"), name='index'),
     url(r'^test/$', TemplateView.as_view(template_name="quiver.html")),
->>>>>>> 2cff87c0eaf1f2a67b89e2e47c58723f20d6d494
                        url(r'^signup/$', TemplateView.as_view(template_name="signup.html"), name="signup"),
                        url(r'^help/$', TemplateView.as_view(template_name="help.html"), name="help"),
                        url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
